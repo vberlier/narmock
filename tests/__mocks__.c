7fec17103916aed39ecfbd5bcbe6b4795f65f64c
Valentin Berlier <berlier.v@gmail.com>
2019-07-21 16:09:48 +0200

    Version 0.1.6

--- tests/__mocks__.c
+++ tests/__mocks__.c
@@ -1,7 +1,7 @@
 /*
 Mocks source file
 
-Generated with Narmock v0.1.5 (https://github.com/vberlier/narmock)
+Generated with Narmock v0.1.6 (https://github.com/vberlier/narmock)
 Do not edit manually
 */
 
