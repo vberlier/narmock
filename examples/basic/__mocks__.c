03765b51c51fbb38c47e3a7c25f9a12149bb864d
Valentin Berlier <berlier.v@gmail.com>
2019-10-13 16:02:33 +0200

    Version 0.2.9

--- examples/basic/__mocks__.c
+++ examples/basic/__mocks__.c
@@ -1,7 +1,7 @@
 /*
 Mocks source file
 
-Generated with Narmock v0.2.8 (https://github.com/vberlier/narmock)
+Generated with Narmock v0.2.9 (https://github.com/vberlier/narmock)
 Do not edit manually
 */
 
