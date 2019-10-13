e53f77017147709c8a30a77d9e3d35bd4507c843
Valentin Berlier <berlier.v@gmail.com>
2019-10-13 15:47:43 +0200

    Version 0.2.8

--- tests/__mocks__.h
+++ tests/__mocks__.h
@@ -1,7 +1,7 @@
 /*
 Mocks header file
 
-Generated with Narmock v0.2.7 (https://github.com/vberlier/narmock)
+Generated with Narmock v0.2.8 (https://github.com/vberlier/narmock)
 Do not edit manually
 */
 
